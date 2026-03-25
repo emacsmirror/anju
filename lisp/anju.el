8967373b1c6adc7169ac0ec87b26235e3738e47e
Charles Choi <kickingvegas@gmail.com>
2026-03-25 10:38:50 -0700

    Add package-lint, fix anju URL
    * Add package-lint target.
    * Fix anju URL.

--- lisp/anju.el
+++ lisp/anju.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2026 Charles Choi
 
 ;; Author: Charles Choi <charles.choi@yummymelon.com>
-;; URL: https://github.com/kickingvegas/casual
+;; URL: https://github.com/kickingvegas/anju
 ;; Keywords: tools
 ;; Version: 1.0.3-rc.1
 ;; Package-Requires: ((emacs "29.1") (magit "4.4.0") (casual "2.14.0") (markdown-mode "2.7"))
